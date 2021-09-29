810dbc5c71fa7d2e4810b662dcfbbef235d5e6eb
prayas pikalmunde <prayaspikalmunde@gmail.com>
2021-09-29 16:51:41 +0530

    first springboot commit

---
+++ src/test/java/com/example/demo/Prayas1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prayas1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
